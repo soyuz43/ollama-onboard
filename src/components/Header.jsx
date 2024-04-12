93e6838d72c82cb08853a220db9cf6df69709139
William R. Stetar <kebekad673@proton.me>
2024-04-11 23:01:55 -0500

    just messing around right now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <header>
-      <h1>My Website</h1>
+      <h1>Homepage</h1>
       <nav>
         <Link to="/">Home</Link>
       </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,24 @@ import './StylesComponent/Home.css';
 
 export const Home = () => {
   return (
+    <>
     <div className="home">
       <Link to="/page-one" className="rectangle">
         <article>
-          <h2>I don't know Where To Start</h2>
-          <p>If you're unsure on how use this website, click me!</p>
+          <h2>Page One</h2>
+          <p>Discover the essentials of our first major topic, including key insights and introductory content.</p>
         </article>
       </Link>
+      </div>
+      <div className="home">
       <Link to="/page-two" className="rectangle">
         <article>
           <h2>Page Two</h2>
           <p>Explore advanced concepts with detailed discussions on more complex subjects and applications.</p>
         </article>
       </Link>
+      </div>
+      <div className="home">
       <Link to="/page-three" className="rectangle">
         <article>
           <h2>Page Three</h2>
@@ -25,5 +30,6 @@ export const Home = () => {
         </article>
       </Link>
     </div>
+    </>
   );
 };
